3caa22d6498344212814d7aa9e794852da384d6b
Arina Manukyan <arimanuk@c2r1s3.42yerevan.am>
2025-04-17 22:28:15 +0400

    sortern enq avelacrel

--- validation.c
+++ validation.c
@@ -1,12 +1,68 @@
 #include "push_swap.h"
 
+void sa(stack **a)
+{
+	int num;
+
+	if (!a || !(*a) || (*a)->next == NULL)
+		return ;
+	num = (*a)->data;
+	(*a)->data = (*a)->next->data;
+	(*a)->next->data = num;
+}
+void sb(stack **b)
+{
+	int num;
+
+	if (!b || !(*b) || (*b)->next == NULL)
+		return ;
+	num = (*b)->data;
+	(*b)->data = (*b)->next->data;
+	(*b)->next->data = num;
+}
+
+void pb(stack **a, stack **b)
+{
+	stack *tmp;
+
+	if ((*a) != NULL)
+	{
+		tmp = *a;
+		*a = (*a)->next;
+		tmp->next = NULL;
+		if (!(*b))
+			*b = tmp;
+		else
+			{
+				tmp->next = *b;
+				*b = tmp;
+			}
+	}
+}
+
+void pa(stack **a, stack **b)
+{
+	stack *tmp;
+
+	tmp = *b;
+	*b = (*b)->next;
+	tmp->next = NULL;
+	if (!(*b))
+		*b = tmp;
+	else
+		{
+			tmp->next = *a;
+			*a = tmp;
+		}
+
+}
+
 void add_back(stack *node, stack **a)
 {
 	stack *tmp;
 
 	if (*a == NULL)
 	{
-		printf("eeee\n");
 		*a = node;
 		return ;
 	}
@@ -34,9 +90,11 @@ int main(int argc, char** argv)
 	int res = 0;
 	char** str = NULL;
 	stack* a;
+	stack* b;
 	stack* node;
 
 	a = NULL;
+	b = NULL;
 	if (argc >= 2)
 	{
 		while(argv[i])
@@ -52,14 +110,27 @@ int main(int argc, char** argv)
 			j = 0;
 			i++;
 		}
+		pb(&a, &b);
+		pb(&a, &b);
+		pb(&a, &b);
+		pb(&a, &b);
+		pb(&a, &b);
+
 	}
 	else
 		write(1, "Error\n", 6);
 	while(a != NULL)
 	{
 		i = a->data;
-		printf("data-> %d\n", i);
+		printf("a-i data-> %d\n", i);
 		a = a->next;
+	}
+	write (1, "\n", 1);
+	while(b != NULL)
+	{
+		i = b->data;
+		printf("b-i data-> %d\n", i);
+		b = b->next;
 	}
 	
 	return(0);
